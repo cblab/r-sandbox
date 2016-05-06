2ed6459a2378ad1a0f10f714d0e8c2eb2018c4b1
Christian Blab <chris.blab@gmail.com>
2016-05-06 09:03:11 +0200

    csv read and write example

--- iris/csv.R
+++ iris/csv.R
@@ -1,4 +1,12 @@
 # create dataframe df1 
 # save it as .csv file with write.csv()
 # load dataframe from file with read.csv()
-
+# csvData.csv has to be touched first 
+var1 <- 1:5
+var2 <- (1:5)/10 
+var3 <- c("R","Machine Learning","Data Mining","Statistics","Analytics")
+df1  <- data.frame(var1, var2, var3)
+names(df1) <- c("Integers", "Real", "Characters")
+write.csv(df1, "~/R/r-sandbox/iris/data/csvData.csv",row.names = FALSE)
+df2 <- read.csv("~/R/r-sandbox/iris/data/csvData.csv")
+print(df2)
