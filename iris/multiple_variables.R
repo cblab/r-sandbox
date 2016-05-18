8cd745b5ace9c4fc06c3602d73afbaf41c3ced00
Christian Blab <chris.blab@gmail.com>
2016-05-18 17:00:59 +0200

    covariance added

---
+++ iris/multiple_variables.R
@@ -0,0 +1,7 @@
+# covariance and correlation between sepal length and petal length
+#Covariance is a measure of how changes in one variable are associated with changes in a second variable.
+#Specifically, covariance measures the degree to which two variables are linearly associated.
+#However, it is also often used informally as a general measure of how monotonically related two variables are.
+
+cov(iris$Sepal.Length, iris$Petal.Length)
+#[1] 1.274315
